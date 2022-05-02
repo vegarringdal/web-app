3a262739c78c827986026073a193c3e8dc0574f3
Vegar Ringdal <vegar.ringdal@gmail.com>
2022-05-02 21:17:57 +0200

    chore: add sql for rest api views

---
+++ sql/011_AI_WEB_REST_API_LIST.sql
@@ -0,0 +1,47 @@
+CREATE OR REPLACE VIEW AI_WEB_REST_API_LIST AS
+    SELECT
+        A.ID,
+        A.NAME,
+        A.ENABLED,
+        A.CREATED,
+        A.CREATED_BY,
+        A.MODIFIED,
+        A.MODIFIED_BY
+    FROM
+        T_WEB_REST_API A;
+
+CREATE OR REPLACE TRIGGER TR_AI_WEB_REST_API_LIST INSTEAD OF
+    INSERT OR UPDATE OR DELETE ON AI_WEB_REST_API_LIST
+    FOR EACH ROW
+BEGIN 
+    -- INSERTING
+    IF INSERTING THEN
+        INSERT INTO T_WEB_REST_API (
+            NAME,
+            ENABLED
+        ) VALUES (
+            :NEW.NAME,
+            :NEW.ENABLED
+        );
+
+    END IF;
+
+    -- UPDATING
+    IF UPDATING THEN
+        UPDATE T_WEB_REST_API
+        SET
+            NAME = :NEW.NAME,
+            ENABLED = :NEW.ENABLED
+        WHERE
+            ID = :OLD.ID;
+
+    END IF;
+
+    -- DELETING
+    IF DELETING THEN
+        DELETE FROM T_WEB_REST_API
+        WHERE
+            ID = :OLD.ID;
+
+    END IF;
+END;
