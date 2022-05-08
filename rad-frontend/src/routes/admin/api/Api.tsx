c5f51a65ddbb22f5a4813419eeff2c07240bf5fe
Vegar Ringdal <vegar.ringdal@gmail.com>
2022-05-08 20:51:17 +0200

    chore: update api admin page

--- rad-frontend/src/routes/admin/api/Api.tsx
+++ rad-frontend/src/routes/admin/api/Api.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import json5 from "json5";
 
 // TODO: I need to imporve this..
-import { verifyApiConfig } from "../../../../../rad-common/src/utils/verifyApiConfig";
+import { verifyApiConfig } from "@rad-common";
 
 export function Api() {
     const controllerName = "WEB_REST_API";
@@ -31,18 +31,19 @@ export function Api() {
 }
 
 export function Details(props: { controllerName: string }) {
-    const [data, setData] = useState({} as { NAME: string; DATA: string });
-    const [error, setError] = useState({} as any);
+    const [data, setData] = useState({} as { ID?: number; NAME: string; DATA: string });
+    const [error, setError] = useState("");
     const controller = getDataControllerByName(props.controllerName);
     const dataSource = controller.dataSource;
 
-    type Details = { NAME: string; DATA: any };
+    type Details = { ID?: number; NAME: string; DATA: any };
 
     useEffect(() => {
         const datahandler = (e: { type: string; data: any }) => {
             if (e?.type === "currentEntity") {
                 // generate new obj, so state see change
                 const obj = {} as Details;
+                obj.ID = dataSource.currentEntity?.ID;
                 obj.NAME = dataSource.currentEntity?.NAME;
                 obj.DATA = dataSource.currentEntity?.DATA;
 
@@ -62,8 +63,7 @@ export function Details(props: { controllerName: string }) {
 
     return (
         <div className="flex flex-col m-2 p-2 flex-1">
-            <span className="p-2 m-2">API name: {data?.NAME}</span>
-            <hr></hr>
+            <span className="p-2 m-2">API ID: {data?.ID}</span>
             <button
                 onClick={() => {
                     if (dataSource.currentEntity) {
@@ -71,37 +71,43 @@ export function Details(props: { controllerName: string }) {
                         const [api, apiError, errorCount] = verifyApiConfig(parsed);
                         if (errorCount) {
                             setError(json5.stringify(apiError, { space: 4 }));
+                            dataSource.currentEntity.DATA = json5.stringify(api);
+                        } else {
+                            setError("OK");
                         }
-                        dataSource.currentEntity.DATA = json5.stringify(api);
                     }
                 }}
                 className="m-2 p-2 bg-gray-200 w-28  hover:bg-gray-300 focus:outline-none  dark:bg-gray-700  dark:hover:bg-gray-600 dark:text-blue-400 font-semibold"
             >
                 Verify & Update
             </button>
-            <span className="p-2 m-2 mb-0">API config json string:(use code IDE for editing..)</span>
-            {/* 
-                I need to make GUI interface or add better code editor: https://github.com/antonmedv/codejar
-            */}
-            <textarea
-                value={data?.DATA || ""}
-                onChange={(e) => {
-                    const obj = Object.assign({}, data);
-                    obj.DATA = e.target.value;
-                    setData(obj);
-                }}
-                onInput={(e: any) => {
-                    const obj = Object.assign({}, data);
-                    obj.DATA = e.target.value;
-                    setData(obj);
-                }}
-                className="flex-1 p-2 m-2 mt-0  bg-gray-700 text-gray-200 outline-none"
-            ></textarea>
-            {error ? "Errors:" : ""}
-            <textarea
-                value={error || ""}
-                className="flex-1 p-2 m-2 mt-0  bg-gray-700 text-gray-200 outline-none"
-            ></textarea>
+            <div className="flex  flex-1">
+                <div className="flex flex-col flex-1">
+                    <span className="p-2 m-2 mb-0">Api Config JSON: </span>
+                    <textarea
+                        value={data?.DATA || ""}
+                        onChange={(e) => {
+                            const obj = Object.assign({}, data);
+                            obj.DATA = e.target.value;
+                            setData(obj);
+                        }}
+                        onInput={(e: any) => {
+                            const obj = Object.assign({}, data);
+                            obj.DATA = e.target.value;
+                            setData(obj);
+                        }}
+                        className="flex-1 p-2 m-2 mt-0  bg-gray-700 text-gray-200 outline-none"
+                    ></textarea>
+                </div>
+                <div className="flex flex-col  flex-1">
+                    <span className="p-2 m-2 mb-0">Verififcation errors:</span>
+                    <textarea
+                        readOnly={true}
+                        value={error || ""}
+                        className="flex-1 p-2 m-2 mt-0  bg-gray-700 text-gray-200 outline-none"
+                    ></textarea>
+                </div>
+            </div>
         </div>
     );
 }
